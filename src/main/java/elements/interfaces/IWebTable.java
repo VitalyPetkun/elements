4ee67731c130967cde3a05822a936a44da28de60
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-04-18 05:29:47 +0300

    Add file WebTable.java
    (cherry picked from commit 50afb6696abfae37c90a4e452d76a8b639bd2f5d)
    (cherry picked from commit 0ac1c08699385a025c9380db8daae6984c2ce21f)

--- src/main/java/elements/interfaces/IWebTable.java
+++ src/main/java/elements/interfaces/IWebTable.java
@@ -2,16 +2,26 @@ package elements.interfaces;
 
 import aquality.selenium.elements.interfaces.IElement;
 
+import java.util.List;
+
 public interface IWebTable extends IElement {
     String getHeadCell(int column);
 
     String getCell(int row, int column);
 
-    String[] getHead();
+    List<String> getHead();
 
-    String[] getBodyRow(int row);
+    List<String> getBodyRow(int row);
 
-    String[][] getBody();
+    List<List<String>> getBody();
+
+    <T extends IElement> List<String> getList(List<T> list);
+
+    <T extends IElement> List<List<String>> getBodyList(List<T> list);
+
+    <T extends IElement> List<IElement> getBodyListElements();
+
+    <T extends IElement> List<IElement> getHeadListElements();
 
     int getColumnSize();
 
