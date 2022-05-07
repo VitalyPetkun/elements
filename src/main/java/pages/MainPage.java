026b366d2ffe6c00086884e86077c8be56fb6984
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-05-07 15:39:36 +0300

    Add file MainPageSteps.java
    (cherry picked from commit ffbd8d376b9bc3ab9fc25164a8955a8232845c33)

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import aquality.selenium.elements.interfaces.ILink;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
@@ -7,8 +8,14 @@ public class MainPage extends Form {
 
     private static final String MAIN_PAGE_LOCATOR = "//div[@class='main-content-wrapper']";
 
+    private final ILink currencyRates = getElementFactory().getLink(
+            By.xpath("//a[contains(@href,'currency') and contains(@class,'main-nav_link')]"),"Currency rates");
+
     public MainPage() {
         super(By.xpath(MAIN_PAGE_LOCATOR), "Main page");
     }
 
+    public void clickCurrencyRates() {
+        currencyRates.click();
+    }
 }
