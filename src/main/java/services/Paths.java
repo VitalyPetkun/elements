8d81e9c64873f84296b811c4f0c98754f4ff68a0
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-05-09 01:59:57 +0300

    Add file SelectedDateTest.java
    (cherry picked from commit 6ed975d91a5d102d9597ff90bc6862be9ab2b96b)

--- src/main/java/services/Paths.java
+++ src/main/java/services/Paths.java
@@ -3,7 +3,8 @@ package services;
 public enum Paths {
 
     MAIN_RESOURCES_PATH("src/main/resources/"),
-    TEST_RESOURCES_PATH("src/test/resources/");
+    TEST_RESOURCES_PATH("src/test/resources/"),
+    IMAGES_TESTS("src//test//resources//images//%s.png");
 
     private String path;
 
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,8 +3,16 @@ package tests;
 import browser.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import services.Url;
+import utils.PropertiesManager;
+
+import static services.Paths.MAIN_RESOURCES_PATH;
+import static services.ProjectFiles.CONFIG;
 
 public class BaseTest {
+
+    protected final String MYFIN_URL = PropertiesManager.getValue(MAIN_RESOURCES_PATH.getPath(),
+            CONFIG.getFile(), Url.MYFIN_URL.getUrl());
 
     @BeforeMethod
     protected void setup() {
