184a5bc91195f179409887c86334f24157b3aeca
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-05-30 03:58:13 +0300

    Update elements

--- src/main/java/elements/Image.java
+++ src/main/java/elements/Image.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import services.Paths;
 import utils.SmartLogger;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
@@ -20,14 +19,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Image extends Element implements IImage {
-
     private final String SAVE_IMAGES_PATH = Paths.IMAGES_PATH.getPath();
     private final String FORMAT_NAME = "png";
-
     private String srcAttribute = "src";
     private String src = null;
     private String fullFileName = null;
-
     private URL url = null;
 
     protected Image(By locator, String name, ElementState state) {
@@ -37,95 +33,69 @@ public class Image extends Element implements IImage {
     protected String getElementType() {
         return this.getLocalizationManager().getLocalizedMessage("Image", new Object[0]);
     }
-
     public void save(String imageName) {
         this.save(imageName, SAVE_IMAGES_PATH);
     }
-
     public void save(String imageName, String path) {
         try {
-            ((JavascriptExecutor) AqualityServices.getBrowser().getDriver())
-                    .executeScript("arguments[0].scrollIntoView();",
-                            AqualityServices.getBrowser().getDriver().findElement(this.getLocator()));
-
+            ((JavascriptExecutor) AqualityServices.getBrowser().getDriver()).executeScript("arguments[0].scrollIntoView();", AqualityServices.getBrowser().getDriver().findElement(this.getLocator()));
             Thread.sleep(2000);
-
             AqualityServices.getBrowser().goTo(this.getUrl().toString());
-
             Robot robot = new Robot();
             robot.keyPress(KeyEvent.VK_CONTROL);
             robot.keyPress(KeyEvent.VK_S);
             robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyRelease(KeyEvent.VK_S);
-
             Thread.sleep(5000);
-
             StringSelection stringSelection = new StringSelection(this.setFullFileName(imageName, path));
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-
             robot.keyPress(KeyEvent.VK_CONTROL);
             robot.keyPress(KeyEvent.VK_V);
             robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyRelease(KeyEvent.VK_V);
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
-
             Thread.sleep(2000);
         } catch (InterruptedException | AWTException e) {
             SmartLogger.logError("Robot not create");
         }
-
         AqualityServices.getBrowser().goBack();
     }
-
-    @Override
     public Boolean compare(String imageName) {
         try {
             BufferedImage image1 = ImageIO.read(new File(this.getFullFileName()));
             BufferedImage image2 = ImageIO.read(new File(imageName));
-
             int columns = image1.getWidth();
             int rows = image1.getHeight();
-
             for (int row = 0; row < rows; row++) {
                 for (int col = 0; col < columns; col++) {
                     int rgb = image1.getRGB(col, row);
                     int rgb2 = image2.getRGB(col, row);
-
                     if (image1.getWidth() != image2.getWidth() || image1.getHeight() != image2.getHeight()) {
                         return false;
                     } else if (rgb != rgb2) {
                         return false;
                     }
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return true;
     }
-
     public String getAttributeSrc() {
         if (src == null)
             src = AqualityServices.getBrowser().getDriver().findElement(this.getLocator()).getAttribute(srcAttribute);
-
         return src;
     }
-
     public String getFullFileName() {
         return fullFileName;
     }
-
     public String setFullFileName(String imageName, String path) {
         if (fullFileName == null)
-            fullFileName = System.getProperty("user.dir").concat("\\")
-                    .concat(path).concat(imageName).concat(".").concat(FORMAT_NAME);
-
+            fullFileName = System.getProperty("user.dir").concat("\\").concat(path).concat(imageName).concat(".").concat(FORMAT_NAME);
         return fullFileName;
     }
-
     private URL getUrl() {
         if (url == null) {
             try {
@@ -134,7 +104,6 @@ public class Image extends Element implements IImage {
                 e.printStackTrace();
             }
         }
-
         return url;
     }
 }
--- src/main/java/elements/WebTable.java
+++ src/main/java/elements/WebTable.java
@@ -6,16 +6,13 @@ import aquality.selenium.elements.interfaces.IElement;
 import aquality.selenium.elements.interfaces.ILink;
 import elements.interfaces.IWebTable;
 import org.openqa.selenium.By;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class WebTable extends Element implements IWebTable {
-
     private final String HEAD_ROW_LOCATOR = "//thead//tr";
     private final String HEAD_CELL_LOCATOR = "//th";
     private final String BODY_CELL_LOCATOR = "//td";
-
     private String BODY_ROW_LOCATOR = "//tbody//tr";
     private List<List<String>> body;
     private List<List<String>> head;
@@ -27,31 +24,25 @@ public class WebTable extends Element implements IWebTable {
     protected String getElementType() {
         return this.getLocalizationManager().getLocalizedMessage("Table", new Object[0]);
     }
-
     public void setBodyRowByXpathLocator(String locator) {
         BODY_ROW_LOCATOR = locator;
     }
-
     public void clickHeadCell(int column) {
         this.getHeadListElements().get(column).clickAndWait();
     }
-
     public void clickCell(int row, int column) {
         List<ILink> rowList = new ArrayList<>();
         rowList.addAll(this.getBodyListElements().get(row).findChildElements(By.xpath(BODY_CELL_LOCATOR), ILink.class));
         rowList.get(column).clickAndWait();
     }
-
     public int getRowIndex(String rowName, List<List<String>> list) {
         int row = -1;
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).contains(rowName))
                 row = i;
         }
-
         return row;
     }
-
     public int getRowNameIndex(String valueCell, int columnIndex, List<List<String>> list) {
         int row = -1;
         for (int i = 0; i < list.size(); i++) {
@@ -60,47 +51,37 @@ public class WebTable extends Element implements IWebTable {
                 break;
             }
         }
-
         return row;
     }
-
     public String getHeadCell(int row, int column) {
         return this.getHead().get(row).get(column);
     }
-
     public String getBodyCell(int row, int column) {
         return this.getBody().get(row).get(column);
     }
-
     public String getBodyCell(String rowName, String columnName) {
         return this.getBody()
                 .get(this.getRowIndex(rowName, this.getBody()))
                 .get(this.getHead().get(0).indexOf(columnName));
     }
-
     public String getBodyRowName(String valueCell, int columnIndex) {
         int rowIndex = this.getRowNameIndex(valueCell, columnIndex, this.getBody());
         return this.getBody().get(rowIndex).get(0);
     }
-
     public List<String> getBodyRow(int row) {
         return this.getBody().get(row);
     }
-
     public List<String> getBodyRow(String rowName) {
         return this.getBody().get(this.getRowIndex(rowName, this.getBody()));
     }
-
     public List<String> getBodyColumn(int columnIndex) {
         List<String> columnList = new ArrayList<>();
         for (List<String> rowList :
                 this.getBody()) {
             columnList.add(rowList.get(columnIndex));
         }
-
         return columnList;
     }
-
     public List<String> getBodyColumn(String columnName) {
         int column = this.getHead().get(0).indexOf(columnName);
         List<String> columnList = new ArrayList<>();
@@ -108,41 +89,31 @@ public class WebTable extends Element implements IWebTable {
                 this.getBody()) {
             columnList.add(rowList.get(column));
         }
-
         return columnList;
     }
-
     public List<ILink> getBodyListElements() {
         return this.findChildElements(By.xpath(BODY_ROW_LOCATOR), "Table body", ILink.class);
     }
-
     public List<ILink> getHeadListElements() {
         return this.findChildElements(By.xpath(HEAD_ROW_LOCATOR), "Table head", ILink.class);
     }
-
     public List<List<String>> getHead() {
         if (head == null)
             head = this.getHeadList(this.getHeadListElements());
-
         return head;
     }
-
     public List<List<String>> getBody() {
         if (body == null)
             body = this.getBodyList(this.getBodyListElements());
-
         return body;
     }
-
     public <T extends IElement> List<String> getList(List<T> list) {
         List<String> newList = new ArrayList<>();
         for (T element : list) {
             newList.add(element.getText());
         }
-
         return newList;
     }
-
     public <T extends IElement> List<List<String>> getHeadList(List<T> list) {
         List<List<String>> newList = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
@@ -151,10 +122,8 @@ public class WebTable extends Element implements IWebTable {
 
             newList.add(this.getList(rowList));
         }
-
         return newList;
     }
-
     public <T extends IElement> List<List<String>> getBodyList(List<T> list) {
         List<List<String>> newList = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
@@ -163,7 +132,6 @@ public class WebTable extends Element implements IWebTable {
 
             newList.add(this.getList(rowList));
         }
-
         return newList;
     }
 }
